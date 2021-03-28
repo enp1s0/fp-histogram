9d575bfeaa050523a10160957f2fc4d0dafff0a6
mutsuki <mutsuki@momo86.net>
2021-03-28 12:55:31 +0900

    Fix the template arguments of `print_histogram` wrapper

--- include/fphistogram/fphistogram.hpp
+++ include/fphistogram/fphistogram.hpp
@@ -97,12 +97,12 @@ void print_histogram(const std::function<T(const std::size_t)> iter, const std::
 template <class T, class Mode = mode_log2>
 void print_histogram(const T* const fp_list, const std::size_t size, const unsigned num_all_stars = 100) {
 	std::function<double(const std::size_t)> iter = [&fp_list](const std::size_t i) {return fp_list[i];};
-	print_histogram(iter, size, num_all_stars);
+	print_histogram<T, Mode>(iter, size, num_all_stars);
 }
 
 template <class T, class Mode = mode_log2>
 void print_histogram(const std::vector<T>& fp_list_vec, const unsigned num_all_stars = 100) {
-	print_histogram(fp_list_vec.data(), fp_list_vec.size(), num_all_stars);
+	print_histogram<T, Mode>(fp_list_vec.data(), fp_list_vec.size(), num_all_stars);
 }
 } // namespace fphistogram
 } // namespace mtk
