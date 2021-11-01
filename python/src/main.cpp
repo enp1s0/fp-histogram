6e9b03ad9cf87b1ab6e581ea5d28939a1f74daaa
mutsuki <mutsuki@momo86.net>
2021-11-01 23:40:57 +0900

    Add setup.py

---
+++ python/src/main.cpp
@@ -0,0 +1,48 @@
+#include <pybind11/pybind11.h>
+#include <pybind11/numpy.h>
+#include "../../include/fphistogram/fphistogram.hpp"
+
+template <class T>
+void print_histogram(
+	pybind11::array_t<T, pybind11::array::c_style | pybind11::array::forcecast> array,
+	const unsigned num_all_stars = 100
+	) {
+	const auto info = array.request();
+	auto ptr = static_cast<T*>(info.ptr);
+
+	// get size
+	unsigned n = 1;
+	for (const auto r : info.shape) {
+		n *= r;
+	}
+
+	// call print func
+	mtk::fphistogram::print_histogram(ptr, n, num_all_stars);
+}
+
+template <class T>
+void print_histogram_pm(
+	pybind11::array_t<T, pybind11::array::c_style | pybind11::array::forcecast> array,
+	const unsigned num_all_stars = 100
+	) {
+	const auto info = array.request();
+	auto ptr = static_cast<T*>(info.ptr);
+
+	// get size
+	unsigned n = 1;
+	for (const auto r : info.shape) {
+		n *= r;
+	}
+
+	// call print func
+	mtk::fphistogram::print_histogram_pm(ptr, n, num_all_stars);
+}
+
+PYBIND11_MODULE(fphistogram, m) {
+    m.doc() = "fp-histogram for Python";
+
+    m.def("print_histogram_double"   , &print_histogram<double>);
+    m.def("print_histogram_float"    , &print_histogram<float>);
+    m.def("print_histogram_pm_double", &print_histogram_pm<double>);
+    m.def("print_histogram_pm_float" , &print_histogram_pm<float>);
+}
