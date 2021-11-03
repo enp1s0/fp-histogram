d0ac86f19b0e22085f2bc7a3ef87184f9cbbaaec
mutsuki <mutsuki@momo86.net>
2021-11-03 18:51:40 +0900

    Add base=10 test

--- python/tests/test01.py
+++ python/tests/test01.py
@@ -2,4 +2,7 @@ import numpy as np
 import fphistogram as fphist
 
 a = np.random.rand(100000)
-fphist.print_histogram(a)
+
+for base in [2, 10]:
+    print("base = ", base)
+    fphist.print_histogram(a, base=base)
